1c9122be5cb5c5bfe6eb79eba60c8b200847e78e
AlexB <alexandru_2487@yahoo.com>
2021-11-11 00:33:56 +0200

    bootstrap css and js

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BattleGame</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap-4.0.0/css/bootstrap.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="screen" />
+    <script src="js/jQuery-3.6.0.js" type="text/javascript"></script>
+    <script src="bootstrap-4.0.0/js/bootstrap.js" type="text/javascript"></script>
+</head>
+<body>
+    <div class="battle--main--container">
+        <b class="battle--story">Battle Story</b>
+        <p>Once upon a time there was a great hero, called Orderus, with some strengths and weaknesses, as all heroes have.</p>
+        <p>As Orderus walks the ever-green forests of Emagia, he encounters some wild beasts.</p>
+        <p>The battle begin.Who will be the winner?</p>
+    </div>
+    <?php
+        require 'vendor/autoload.php';
+
+        
+    ?>
+</body>
+</html>
